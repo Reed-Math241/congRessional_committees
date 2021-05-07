a86cd4109fc46fafe6beaafc5c41090684ec8916
robinhardwick <robin.hardwick28>
2021-05-07 11:38:09 -0700

    All output (including interactive!!) works :)

--- Congressional Committee Shiny/app.r
+++ Congressional Committee Shiny/app.r
@@ -10,6 +10,19 @@ library(readr)
 library(statnet)
 library(ade4)
 library(broom)
+library(networkD3)
+
+# ggplot theme function
+robins_ggplot_theme <- function(font = "Times") {
+  theme_light() +
+    theme(plot.title = element_text(hjust = 0.5, family = font, face = "bold"),
+          plot.subtitle = element_text(hjust = 0.5, face = "italic", family = font),
+          axis.title = element_text(family = font),
+          axis.text = element_text(family = font),
+          legend.title = element_text(family = font),
+          legend.text = element_text(family = font),
+          plot.caption = element_text(family = font, face = "italic"))
+}
 
 # Reading the csvs
 committee_edge_list <- read_csv(here("Congressional Committee Shiny/committee_edge_list.csv")
@@ -43,8 +56,10 @@ ui <- fluidPage(
     ),
     mainPanel(
       # Set up my tabs and what output goes where HERE!
+      plotOutput(outputId = "network"),
       dataTableOutput(outputId = "t_test_table"),
-      plotOutput(outputId = "network")
+      plotOutput(outputId = "boxplot"),
+      forceNetworkOutput(outputId = "interactive")
     )
   )
 )
@@ -154,13 +169,105 @@ server <- function(input, output){
                                                  by = c("IDs" = "name"))
     
     # Once we get things to work, write t.tests into if statements by the input selected
-    # Currently it errors because there are NO factors to do a t.test with
-    gender_deg <- tidy(t.test(congress_deg ~ title_bin, data = congressperson_affiliation_new))
     
-    datatable(gender_deg,
+    if(input$color == "Gender") {
+      hyp_test_degree <- tidy(t.test(congress_deg ~ gender_bin, data = congressperson_affiliation_new))
+      hyp_test_eig <- tidy(t.test(congress_eig ~ gender_bin, data = congressperson_affiliation_new))
+    }
+    else if(input$color == "Leadership") {
+      hyp_test_degree <- tidy(t.test(congress_deg ~ title_bin, data = congressperson_affiliation_new))
+      hyp_test_eig <- tidy(t.test(congress_eig ~ title_bin, data = congressperson_affiliation_new))
+    } else {
+      hyp_test_degree <- tidy(t.test(congress_deg ~ party_bin, data = congressperson_affiliation_new))
+      hyp_test_eig <- tidy(t.test(congress_eig ~ party_bin, data = congressperson_affiliation_new))
+    }
+    
+    hyp_test <- rbind(hyp_test_degree, hyp_test_eig)
+    hyp_test <- hyp_test %>%
+      rowid_to_column("id") %>%
+      mutate(Centrality_Measure = case_when(id == 1 ~ "Degree Centrality",
+                                            id == 2 ~ "Eigenvector Centrality")) %>%
+      select(-id) %>%
+      select(Centrality_Measure, everything())
+    datatable(hyp_test,
               options = list(paging = FALSE,
                              searching = FALSE,
                              orderClasses = TRUE))
+  })
+  
+  output$boxplot <- renderPlot({
+    congressperson_jaccard <- dist.binary(t(bipartite_matrix), 
+                                         method = 1, upper = TRUE, 
+                                         diag = FALSE)
+    congressperson_jaccard <- as.matrix(congressperson_jaccard)   
+    diag(congressperson_jaccard) <- 0
+    congressperson_jaccard <- ifelse(congressperson_jaccard > 0.95, 1, 0)
+    jacc_congressperson <- as.network(congressperson_jaccard,    # Create a statnet network
+                                      directed = FALSE)
+    
+    IDs <- jacc_congressperson%v%"vertex.names"
+    congress_deg  <- degree(jacc_congressperson)
+    #congress_bet  <- betweenness(jacc_congressperson)
+    #congress_clos <- closeness(jacc_congressperson,
+    #             cmode="suminvdir")
+    congress_eig  <- evcent(jacc_congressperson)
+    
+    congress_cent_df <- data.frame(IDs, 
+                                   congress_deg, 
+                                   #congress_bet, 
+                                   #congress_clos, 
+                                   congress_eig)
+    
+    congressperson_affiliation_new <- full_join(congress_cent_df, congressperson_affiliation_reactive(), 
+                                                by = c("IDs" = "name"))
+    
+    # Make a column of centrality, facet by type
+    congressperson_affiliation_box <- pivot_longer(congressperson_affiliation_new,
+                                                   cols = c("congress_deg", "congress_eig"),
+                                                   names_to = "Name_of_Centrality_Measure",
+                                                   values_to = "Centrality_Measure")
+    if(input$color == "Gender") {
+      ggplot(data = congressperson_affiliation_box, mapping = aes(x = factor(gender_bin),
+                                                                  y = Centrality_Measure)) +
+        geom_boxplot() +
+        facet_wrap(~Name_of_Centrality_Measure, scales = "free") +
+        robins_ggplot_theme()
+    }
+    else if(input$color == "Leadership") {
+      ggplot(data = congressperson_affiliation_box, mapping = aes(x = factor(title_bin),
+                                                                  y = Centrality_Measure)) +
+        geom_boxplot() +
+        facet_wrap(~Name_of_Centrality_Measure, scales = "free") +
+        robins_ggplot_theme()
+    } else {
+      ggplot(data = congressperson_affiliation_box, mapping = aes(x = factor(party_bin),
+                                                                  y = Centrality_Measure)) +
+        geom_boxplot() +
+        facet_wrap(~Name_of_Centrality_Measure, scales = "free") +
+        robins_ggplot_theme()
+    }
+  })
+  
+  output$interactive <- renderForceNetwork({
+    # Making my matrix into an edge list
+    m <- matrix_prod
+    idx = m > 0.001
+    edges = data.frame(
+      source = row(m)[idx],
+      target = col(m)[idx],
+      corr = m[idx])
+    
+    # Transform it to match the d3 network requirements
+    nodes_d3 <- mutate(congressperson_affiliation_reactive(), id = id - 1)
+    edges_d3 <- mutate(edges, source = source - 1, target = target - 1)
+    
+    ColourScale <- 'd3.scaleOrdinal()
+            .domain(["blue", "pink2"])
+           .range(["blue", "pink"]);'
+    
+    forceNetwork(Links = edges_d3, Nodes = nodes_d3, Source = "source", Target = "target", 
+                 NodeID = "name", Group = "gender_color", Value = "corr", 
+                 opacity = 1, fontSize = 16, zoom = TRUE, colourScale = JS(ColourScale))
   })
   
 }
